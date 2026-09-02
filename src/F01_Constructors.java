b15b56ad4ed3b45a98ee050829c2d3eacf0e830f
MunjurKadir <Munjurkadir656@gmail.com>
2026-09-02 15:29:30 +0600

    Java Constructors

--- src/F01_Constructors.java
+++ src/F01_Constructors.java
@@ -5,7 +5,7 @@ class Main {
     int x;
     Main (){
         x = 5;
-    }
+    } // Constructor
     public static void main (String[] args){
         Main myObj = new Main();
         System.out.println(myObj.x);
@@ -18,19 +18,36 @@ class Main1 {
     int x;
     Main1(int y) {
         x = y;
-    }
+    } // Constructor
 
 }
 class Car {
     int modelyear;
     String modelname;
-    Car (int year, String name){
+    Car (int year, String name){ // Constructor
         modelyear = year;
         modelname = name;
     }
     public static void main (String[] args){
         Car myCar = new Car(1986, "Honda");
         System.out.println(myCar.modelyear + " " + myCar.modelname);
+    }
+}
+class Student {
+    String name;
+    int age;
+    Student(String n, int a){ // Constructor
+        name = n;
+        age = a;
+    }
+    public static void main (String[] args){
+        Student s1 = new Student("Shawon", 22);
+        Student s2 = new Student("Rakib", 21);
+        Student s3 = new Student("Mim", 23);
+
+        System.out.println("Name: " + s1.name + "\nAge: " + s1.age);
+        System.out.println("Name: " + s2.name + "\nAge: " + s2.age);
+        System.out.println("Name: " + s3.name + "\nAge: " + s3.age);
     }
 }
 
